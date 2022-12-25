f70dec6a5f57b5322924f2c074002be50e9ce0fd
Bendako <62848655+Bendako@users.noreply.github.com>
2022-12-26 01:12:56 +0200

    Add Links

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,8 +2,8 @@ export const projects = [
   {
     title: 'Project #1',
     description: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
+    image: '/images/1.png',
+    tags: ['Mongo', 'Express', 'React', 'Node'],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 0,
